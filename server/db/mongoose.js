c9e5ed853f62798db214b82a8bbc95e12d39e9d5
marten <marten@modernwomenmedia.se>
2018-03-29 15:57:40 +0200

    Changed MONGODB_URI to PORT

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-if (process.env.MONGODB_URI) {
+if (process.env.PORT) {
   mongoose.connect('mongodb://admin:9wkDd66b4xQ8)XQF8*@ds227459.mlab.com:27459/node-todo-api');
 } else {
   mongoose.connect('mongodb://localhost:27017/TodoApp');
