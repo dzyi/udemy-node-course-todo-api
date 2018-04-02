5a5be802c8633d48eef070f33142cc40892da74c
marten <marten@modernwomenmedia.se>
2018-04-03 00:44:53 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('Decoded: ', decoded);
+var hashedPassword = '$2a$10$H/NWhNggi8moCTzRmJZlP.Kpj/C15ruGab1r8w0cSsdDnbO.cIBZi';
+
+var hashedPassword2 = '$2a$10$YVKirkhC57RpkjT9hpKM5uUaLaodSoUmeuzGVdEpmUsANDIgx09VW';
+
+bcrypt.compare('123', hashedPassword2, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('Decoded: ', decoded);
 
 // var message = 'I am user number 3';
 //
