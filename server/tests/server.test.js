98a45e39a68ded36fa6583b18242965abed03cfb
marten <marten@modernwomenmedia.se>
2018-04-03 11:44:25 +0200

    syntax edit

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,18 +5,15 @@ const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: 'First test todo'
-  },
-  {
-    _id: new ObjectID(),
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 333
-  }
-];
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
+}];
 
 beforeEach((done) => {
   Todo.remove({}).then(() => {
